ddc4ba11df0d0fd78583ed4408a11bb30988f03e
Raindu <101939207+Raindu@users.noreply.github.com>
2026-02-11 20:56:21 +0530

    api added to url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_URL = process.env.VITE_API_URL || 'http://localhost:5000/api';
 // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-const API_URL = import.meta.env.REACT_APP_API_URL || 'https://ink-blog-backend.onrender.com';
+const API_URL = import.meta.env.REACT_APP_API_URL || 'https://ink-blog-backend.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
